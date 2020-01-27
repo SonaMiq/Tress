ce9949fbdd10dd844c22a71d8ea91c95751dec6a
Your Name <sonmiq@gmail.com>
2020-01-27 13:49:43 +0400

    fale modified

---
+++ src/ThreadOrderMain.java
@@ -0,0 +1,7 @@
+public class ThreadOrderMain {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadOrder threadOrder1=new ThreadOrder("T1");
+        ThreadOrder threadOrder2=new ThreadOrder("T2");
+        ThreadOrder threadOrder3=new ThreadOrder("T3");
+    }
+}
